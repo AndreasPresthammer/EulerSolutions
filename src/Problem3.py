c7c32fa3ba141bca0b3bfb0d6759f03f72b385e1
Andreas Presthammer <andhaug@gmail.com>
2012-03-29 23:30:27 +0200

    Solved Problem 3

--- src/Problem3.py
+++ src/Problem3.py
@@ -1,35 +1,53 @@
 import unittest
-import math
+import operator
+# lrange stolen from http://stackoverflow.com/questions/2187135/python-range-and-xrange-for-13-digit-numbers
+def lrange(num1, num2 = None, step = 1):
+    op = operator.__lt__
 
-def solution(below):
-    sum = 0
-    prevprev = 0
-    prev = 1
-    next = 0
-    while next < below:
-        next = prevprev + prev
-        
-        if next % 2 == 0:
-            sum += next
-        
-        prevprev = prev
-        prev = next
-    return sum
+    if num2 is None:
+        num1, num2 = 0, num1
+    if num2 < num1:
+        if step > 0:
+            num1 = num2
+        op = operator.__gt__
+    elif step < 0:
+        num1 = num2
 
-def findPrime(number):
-    for i in range(3, math.sqrt(number)):
-        if i%2==0:
+    while op(num1, num2):
+        yield num1
+        num1 += step
+
+def getFactors(number):
+    n = number
+    for i in lrange(2, n + 1):
+        print i
+        
+        while(n % i == 0):
             yield i
-
+            n /= i
+        
+        if n == 1:
+            break
+        
 class Tests(unittest.TestCase):
     
-    def testSimple(self):
-        for item in findPrime(10):
-            print item
-#        result = solution(13195)
-#        self.assertEqual(29, result)
+    def testEulerSolution(self):
+        number = 600851475143
         
-#    def testFull(self):
-#        result = solution(4000000)
-#        self.assertEqual(4613732, result)
-        
+        print list(getFactors(number))
+    
+#    def testPrimeFactorization1(self):
+#        number = 13195
+#        expectedFactors = [5,7,13,29]
+#        
+#        self.assertEqual(expectedFactors, list(getFactors(number)))
+#
+#    def testNumberIsPrime_5_ShouldReturn_5(self):
+#        expectedFactors = [5]
+#        
+#        self.assertEqual(expectedFactors, list(getFactors(5)))
+#        
+#    def testNumberIsPrime_4_ShouldReturn_2x2(self):
+#        expectedFactors = [2, 2]
+#        
+#        self.assertEqual(expectedFactors, list(getFactors(4)))
